eab8e78cc8a9d38a20eeb0afb08ea67ba74f9196
Md. Rakibul Haque <remon.rakibul.star@gmail.com>
2021-12-04 17:28:57 +0600

    local config

--- covid19app/urls.py
+++ covid19app/urls.py
@@ -23,8 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('predict/', views.predict, name='predict'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
